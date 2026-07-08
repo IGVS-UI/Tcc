151197c59aa4ee1c7f91b42dc6b6867be02a2c86
IGVS-UI <igorgomessilva128@gmail.com>
2026-07-08 20:32:35 -0300

    commit

--- assets/sistema-login/js/auth.js
+++ assets/sistema-login/js/auth.js
@@ -24,12 +24,12 @@ import {
 } from "./routes.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOfHsJq2lBKq87n4PYL1aq1cLrhkC0wIg",
+  apiKey: "AIzaSyCOfHsJq2lBKq87n4PYL1aq1cLrhkC0wIgfg",
   authDomain: "tcc-7491c.firebaseapp.com",
   projectId: "tcc-7491c",
-  storageBucket: "tcc-7491c.firebasestorage.app",
+  storageBucket: "tcc-7491c.firebasestorasdfsfdgge.app",
   messagingSenderId: "626560679583",
-  appId: "1:626560679583:web:9514f3b9c9a4c1fa26acaa",
+  appId: "1:626560679583:web:9514f3b9c9a4c1fa26acsdfsfdaa",
   measurementId: "G-Y76EJYVFMN"
 };
 
